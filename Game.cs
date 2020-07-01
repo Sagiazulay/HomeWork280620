26cbf839297a39051b250adb74d570c146d2007e
Sagiazulay <58825185+Sagiazulay@users.noreply.github.com>
2020-07-01 20:36:12 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork280620
+{
+    public class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+                
+        }
+        public Game(string name, float rating)
+        {
+            _name = name;
+            _rating = rating;
+        }
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"Gamer Name : {_name} Gamer Rating : {_rating} Country : {_originCountry} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork280620
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game CandyCrash = new Game();
+
+            Game Bubbles = new Game(name: "Bubbles", rating: 5.5f);
+
+            Game Sodoko = new Game(name: "SOdoko", originCountry: "Israel");
+
+            Game Soccer = new Game(name: "Soccer", numberOfPlayers: 11, rating : 5, originCountry: "USA");
+
+            PrintGame(Bubbles);
+            PrintGame(Soccer);
+        }
+        static void PrintGame(Game g)
+        {
+            Console.WriteLine(g);
+            int number = g.TellMeHowManyPlayers();
+            if (number > 0)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("There is no more than 0 players");
+            }
+
+        }
+    }
+}
